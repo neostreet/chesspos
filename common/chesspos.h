95eda33cca214aaabc636dabf0687f3b89dce964
neostreet <lloyd.aidan@gmail.com>
2025-01-09 23:05:49 -0500

    add #define for the File and View menu items

--- common/chesspos.h
+++ common/chesspos.h
@@ -1,9 +1,15 @@
-#define IDM_EXIT               108
+#define IDM_NEW                100
+#define IDM_OPEN               101
+#define IDM_SAVE               102
+#define IDM_SAVEAS             103
+#define IDM_EXIT               104
 
-#define IDI_CHESSPOS          109
-#define IDI_SMALL              110
+#define IDI_CHESSPOS           105
+#define IDI_SMALL              106
 
-#define IDM_ABOUT              214
+#define IDM_TOGGLE_ORIENTATION 107
+
+#define IDM_ABOUT              108
 
 #define IDC_STATIC -1
 
