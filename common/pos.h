7dc07921b4421c28dc9a6e819d97768ce1069dd8
neostreet <lloyd.aidan@gmail.com>
2025-01-11 18:02:08 -0500

    change orientation to a char in the game_position struct

--- common/pos.h
+++ common/pos.h
@@ -46,6 +46,6 @@
 #define ANNOTATION_Y 5
 
 struct game_position {
-  int orientation;
+  char orientation;
   unsigned char board[CHARS_IN_BOARD];  /* 8 columns * 8 rows / 2 (nibbles per char) */
 };
