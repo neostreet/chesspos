829e38f7b9652c840832ba089c60847539574c00
neostreet <lloyd.aidan@gmail.com>
2025-04-17 12:53:25 -0400

    add IDM_SAVEAS_B

--- common/chesspos.h
+++ common/chesspos.h
@@ -2,16 +2,17 @@
 #define IDM_OPEN               101
 #define IDM_SAVE               102
 #define IDM_SAVEAS             103
-#define IDM_EXIT               104
+#define IDM_SAVEAS_B           104
+#define IDM_EXIT               105
 
-#define IDI_CHESSPOS           105
-#define IDI_SMALL              106
+#define IDI_CHESSPOS           106
+#define IDI_SMALL              107
 
-#define IDM_TOGGLE_ORIENTATION 107
+#define IDM_TOGGLE_ORIENTATION 108
 
-#define IDM_TOGGLE_MOVE_MODE   108
+#define IDM_TOGGLE_MOVE_MODE   109
 
-#define IDM_ABOUT              109
+#define IDM_ABOUT              110
 
 #define IDC_STATIC -1
 
